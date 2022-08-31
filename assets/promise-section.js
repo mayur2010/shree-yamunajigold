8a446ac84044cb18cc4e76115ad6a81b2f7a7255
Mayur Patel <mayurgodhani2010@gmail.com>
2022-08-31 13:54:24 +0530

    update

---
+++ assets/promise-section.js
@@ -0,0 +1,13 @@
+$(document).ready(function()
+    {
+      console.log("ready");
+      $('.incards').hover(
+        function(){ 
+          console.log("hover");
+          $(this).children(".hover-content").addClass("active"); //Add an active class to the anchor
+        },
+        function() {
+          $(this).children(".hover-content").removeClass("active"); //Remove an active class to the anchor
+        }
+     )
+   });
